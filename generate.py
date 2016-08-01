cb740afd398539998b726ad13f3350678b835ca0
Shaunak Kishore <kshaunak@gmail.com>
2016-07-31 22:31:00 -0400

    Add instrument change and document units

--- generate.py
+++ generate.py
@@ -5,12 +5,19 @@ import subprocess
 midi = midiutil.MidiFile.MIDIFile(1)
 
 midi.addTrackName(track=0, time=0, trackName='Track 0')
+
+# `time` is measured in beats. `tempo` is measured in beats per minute.
 midi.addTempo(track=0, time=0, tempo=120)
+
+# 'program' == instrument. Change channel 1 to be something strange.
+midi.addProgramChange(track=0, channel=1, time=0, program=64)
+
+# `duration` is measured in beats.
 midi.addNote(track=0, channel=0, pitch=60, time=0, duration=1, volume=100)
+midi.addNote(track=0, channel=1, pitch=72, time=1, duration=1, volume=100)
 
 with open('output.mid', 'wb') as output:
     midi.writeFile(output)
 
 subprocess.check_call(['timidity', 'output.mid', '-Ow', '-o', 'output.wav'])
-subprocess.check_call(['lame', 'output.wav', '-b', '64', 'output.mp3'])
-subprocess.check_call(['rm', 'output.mid', 'output.wav'])
+subprocess.check_call(['rm', 'output.mid'])
