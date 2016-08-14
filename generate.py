92a6fa16778f09f0b46f27fa34ea980f6e0fede3
Jeff Wu <wuthefwasthat@gmail.com>
2016-08-14 13:27:54 -0700

    switch to sh

--- generate.py
+++ generate.py
@@ -2,12 +2,12 @@
 from pyknon.genmidi import Midi
 from pyknon.music import NoteSeq
 
-import subprocess
+import sh
 
 notes1 = NoteSeq('D4 F#8 A Bb4')
 midi = Midi(1, instrument=40, tempo=90)
 midi.seq_notes(notes1, track=0)
 midi.write('output.mid')
 
-subprocess.check_call(['timidity', 'output.mid', '-Ow', '-o', 'output.wav'])
-subprocess.check_call(['rm', 'output.mid'])
+sh.timidity('output.mid', '-Ow', '-o', 'output.wav')
+sh.rm('output.mid')
