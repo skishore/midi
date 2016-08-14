05a2292a4ae71429b2d47b083746c1cb5f6d5f4a
Shaunak Kishore <kshaunak@gmail.com>
2016-08-14 16:21:43 -0400

    Switch to pyknon

--- generate.py
+++ generate.py
@@ -1,23 +1,13 @@
 #!/usr/local/bin/python
-import midiutil.MidiFile
+from pyknon.genmidi import Midi
+from pyknon.music import NoteSeq
+
 import subprocess
 
-midi = midiutil.MidiFile.MIDIFile(1)
-
-midi.addTrackName(track=0, time=0, trackName='Track 0')
-
-# `time` is measured in beats. `tempo` is measured in beats per minute.
-midi.addTempo(track=0, time=0, tempo=120)
-
-# 'program' == instrument. Change channel 1 to be something strange.
-midi.addProgramChange(track=0, channel=1, time=0, program=64)
-
-# `duration` is measured in beats.
-midi.addNote(track=0, channel=0, pitch=60, time=0, duration=1, volume=100)
-midi.addNote(track=0, channel=1, pitch=72, time=1, duration=1, volume=100)
-
-with open('output.mid', 'wb') as output:
-    midi.writeFile(output)
+notes1 = NoteSeq('D4 F#8 A Bb4')
+midi = Midi(1, instrument=40, tempo=90)
+midi.seq_notes(notes1, track=0)
+midi.write('output.mid')
 
 subprocess.check_call(['timidity', 'output.mid', '-Ow', '-o', 'output.wav'])
 subprocess.check_call(['rm', 'output.mid'])
